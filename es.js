a5924487db300dd014ccfe9150c031879729c80e
Xiaojue <designsor@gmail.com>
2015-01-19 18:40:42 +0800

    add every and isType check

--- es.js
+++ es.js
@@ -2,21 +2,48 @@
  * @author xiaojue [designsor@gmail.com]
  * @fileoverview ES5-6 shim for client javascript
  */
-(function(win, doc, Arr, Str, D, M, Num, Obj, Reg, global, undef) {
+(function(win, doc, Arr, Str, D, M, Num, Obj, Reg, B, global, undef) {
 
   var AP = Arr.prototype,
   SP = Str.prototype,
   DP = D.prototype,
   NP = Num.prototype,
-  RP = Reg.prototype;
+  BP = B.prototype,
+  RP = Reg.prototype,
+  toString = Obj.prototype.toString.call,
+  is = function(type) {
+    return function(val) {
+      return toString(val) === '[object ' + type + ']';
+    };
+  },
+  isArr = is('Array'),
+  isFun = is('Function'),
+  isStr = is('String'),
+  isReg = is('RegExp');
 
   var Arr5 = {
-    isArray: function() {}
+    isArray: isArr
   };
 
   var ArrPro5 = {
-    every: function() {},
-    filter: function() {},
+    //https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/every
+    //http://es5.github.com/#x15.4.4.16
+    every: function(fun, thisP) {
+      var o = new Obj(this),
+      len = O.length >>> 0,
+      k = 0;
+      if (!isFun(fun)) throw new TypeError(fun + ' is not a function');
+      while (k < len) {
+        if (k in O) {
+          if (!fun.call(thisP, O[k], k, O)) return false;
+        }
+        k++;
+      }
+      return true;
+    },
+    filter: function() {
+
+    },
     forEach: function() {},
     indexOf: function() {},
     lastIndexOf: function() {},
@@ -61,9 +88,19 @@
     includes: function() {}
   };
 
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
+  function pad(number) {
+    if (number < 10) return '0' + number;
+    return number;
+  }
+
   var D5 = {
-    now: function() {},
-    toISOString: function() {}
+    now: function() {
+      return new D().getTime();
+    },
+    toISOString: function() {
+      return this.getUTCFullYear() + '-' + pad(this.getUTCMonth() + 1) + '-' + pad(this.getUTCDate()) + 'T' + pad(this.getUTCHours()) + ':' + pad(this.getUTCMinutes()) + ':' + pad(this.getUTCSeconds()) + '.' + (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5) + 'Z';
+    }
   };
 
   var DPro5 = {
@@ -92,6 +129,7 @@
   };
 
   var NumPro5 = {
+    toJSON: function() {},
     toFixed: function() {}
   };
 
@@ -105,6 +143,10 @@
     isInteger: function() {},
     isSafeInteger: function() {},
     isFinite: function() {}
+  };
+
+  var BPro5 = {
+    toJSON: function() {}
   };
 
   var Obj5 = {
@@ -129,7 +171,10 @@
 
   var Global5 = {
     parseInt: function() {},
-    JSON: function() {}
+    JSON: {
+      parse: function() {},
+      stringify: function() {}
+    }
   };
 
   var Global6 = {
@@ -163,12 +208,12 @@
   }
 
   if (!supportES5) {
-    extend([Arr5, Arr], [ArrPro5, AP], [StrPro5, SP], [D5, D], [DPro5, DP], [NumPro5, NP], [Obj5, Obj], [Global5, global]);
+    extend([Arr5, Arr], [ArrPro5, AP], [StrPro5, SP], [D5, D], [DPro5, DP], [NumPro5, NP], [Obj5, Obj], [BPro5, BP], [Global5, global]);
   }
 
   if (!supportES6) {
-    extend([Arr6, Arr], [ArrPro6, AP], [Str6, Str], [StrPro6, SP], [Num6, Num], [M6, M], [Obj6, Obj,true], [RegPro6, RP], [Global6, global, true]);
+    extend([Arr6, Arr], [ArrPro6, AP], [Str6, Str], [StrPro6, SP], [Num6, Num], [M6, M], [Obj6, Obj, true], [RegPro6, RP], [Global6, global, true]);
   }
 
-})(window, document, Array, String, Date, Math, Number, Object, RegExp, this);
+})(window, document, Array, String, Date, Math, Number, Object, RegExp, Boolean, this);
 
