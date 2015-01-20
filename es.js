f81c1e44775c69921b82bb50556b2e3150a7d375
Xiaojue <designsor@gmail.com>
2015-01-20 16:13:39 +0800

    add ArrPro5 method

--- es.js
+++ es.js
@@ -10,16 +10,27 @@
   NP = Num.prototype,
   BP = B.prototype,
   RP = Reg.prototype,
-  toString = Obj.prototype.toString.call,
+  toString = Obj.prototype.toString,
   is = function(type) {
     return function(val) {
-      return toString(val) === '[object ' + type + ']';
+      return toString.call(val) === '[object ' + type + ']';
     };
   },
   isArr = is('Array'),
   isFun = is('Function'),
   isStr = is('String'),
   isReg = is('RegExp');
+
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf
+  function toInteger(num) {
+    var n = Num(num);
+    if (n != num || n === Infinity) { //isNaN,expect string
+      n = 0;
+    } else if (n !== 0 && n != (1 / 0) && n != - (1 / 0)) {
+      n = (n > 0 || - 1) * M.floor(M.abs(n));
+    }
+    return n;
+  }
 
   var Arr5 = {
     isArray: isArr
@@ -48,7 +59,7 @@
       res = [];
       if (!isFun(fun)) throw new TypeError(fun + ' is not a function');
       for (var i = 0; i < len; i++) {
-        if(i in O){
+        if (i in O) {
           var value = O[i];
           if (fun.call(thisP, value, i, O)) res.push(value);
         }
@@ -56,27 +67,115 @@
       return res;
     },
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-    forEach: function(fun,thisP) {
+    forEach: function(fun, thisP) {
       var O = Obj(this),
       len = O.length >>> 0,
       k = 0;
       if (!isFun(fun)) throw new TypeError(fun + ' is not a function');
-      while(k < len){
-        if(k in O){
-          fun.call(thisP,O[k],k,O); 
+      while (k < len) {
+        if (k in O) {
+          fun.call(thisP, O[k], k, O);
         }
         k++;
       }
     },
-    indexOf: function() {
-             
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+    indexOf: function(searchElement, fromIndex) {
+      var O = Obj(this),
+      k,
+      len = O.length >>> 0,
+      n = toInteger(fromIndex);
+      if (len === 0 || n >= len) return - 1;
+      k = M.max(n >= 0 ? n: len - M.abs(n), 0);
+      while (k < len) {
+        if (k in O && O[k] === searchElement) return k;
+        k++;
+      }
+      return - 1;
     },
-    lastIndexOf: function() {},
-    map: function() {},
-    some: function() {},
-    reduce: function() {},
-    reduceRight: function() {}
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf
+    lastIndexOf: function(searchElement, fromIndex) {
+      var O = Obj(this),
+      k,
+      n,
+      len = O.length >>> 0;
+      if (len === 0) return - 1;
+      n = len - 1;
+      if (arguments.length > 1) {
+        n = toInteger(fromIndex);
+      }
+      for (k = n >= 0 ? M.min(n, len - 1) : len - M.abs(n); k >= 0; k--) {
+        if (k in O && O[k] === searchElement) return k;
+      }
+      return - 1;
+    },
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+    map: function(fun, thisP) {
+      var O = Obj(this),
+      len = O.length >>> 0,
+      t,
+      arr = new Arr(len),
+      k = 0;
+      if (!isFun(fun)) throw new TypeError(fun + ' is not a function');
+      while (k < len) {
+        if (k in O) {
+          arr[k] = fun.call(thisP, O[k], k, O);
+        }
+        k++;
+      }
+      return arr;
+    },
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+    some: function(fun, thisP) {
+      var O = Obj(this),
+      len = O.length >>> 0;
+      if (!isFun(fun)) throw new TypeError(fun + ' is not a function');
+      for (var i = 0; i < len; i++) {
+        if (i in O && fun.call(thisP, O[i], i, O)) return true;
+      }
+      return false;
+    },
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+    reduce: function(fun, initialValue) {
+      var O = Obj(this);
+      return reduce(O, fun, initialValue, 'left');
+    },
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduceRight
+    reduceRight: function(fun, initialValue) {
+      var O = Obj(this);
+      return reduce(O, fun, initialValue, 'right');
+    }
   };
+
+  function reduce(source, fun, initialValue, type) {
+    var len = source.length >>> 0,
+    isLeft = type === 'left',
+    k = isLeft ? 0: len - 1,
+    t,
+    index,
+    express,
+    value;
+    if (!isFun(fun)) throw new TypeError(fun + ' is not a function');
+    if (initialValue) {
+      value = initialValue;
+    } else {
+      do {
+        if (k in source) {
+          value = source[k];
+          k += (!!isLeft || -1);
+          break;
+        }
+        index = isLeft ? k + 1: k - 1;
+        express = isLeft ? index >= len: index < 0;
+        if (express) throw new TypeError('Reduce of empty array with no initial value');
+      } while (true);
+    }
+    while (isLeft ? k < len: k >= 0) {
+      if (k in source) value = fun.call(undefined, value, source[k], k, source);
+      k += (!!isLeft || -1);
+    }
+    return value;
+  }
 
   var Arr6 = {
     from: function() {},
@@ -226,17 +325,18 @@
       target = exts[1],
       forceAssign = exts[3];
       for (var j in source) {
-        if (!forceAssign && (j in target)) continue;
+        //if (!forceAssign && (j in target)) continue;
         target[j] = source[j];
       }
     }
   }
 
-  if (!supportES5) {
-    extend([Arr5, Arr], [ArrPro5, AP], [StrPro5, SP], [D5, D], [DPro5, DP], [NumPro5, NP], [Obj5, Obj], [BPro5, BP], [Global5, global]);
-  }
-
+  //if (!supportES5) {
+  console.log(5);
+  extend([Arr5, Arr], [ArrPro5, AP], [StrPro5, SP], [D5, D], [DPro5, DP], [NumPro5, NP], [Obj5, Obj], [BPro5, BP], [Global5, global]);
+  //}
   if (!supportES6) {
+    console.log(6);
     extend([Arr6, Arr], [ArrPro6, AP], [Str6, Str], [StrPro6, SP], [Num6, Num], [M6, M], [Obj6, Obj, true], [RegPro6, RP], [Global6, global, true]);
   }
 
